c519fcc5f1b8705fcd5d0e13bd9f9a1f384d5816
Enderman0609 <hannes.segebarth@gmail.com>
2025-01-08 20:43:36 +0100

    Add Project

---
+++ Assets/PlayerControls.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCtrl : MonoBehaviour
+{
+
+    public float movSpeed;
+    float speedX, speedY;
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();  
+    }
+
+    // Update is called once per frame
+    [System.Obsolete]
+    void Update()
+    {
+        speedX = Input.GetAxis("Horizontal");
+        speedY = Input.GetAxis("Vertical");
+        rb.velocity = new Vector2(speedX * movSpeed, speedY * movSpeed);
+    }
+}
