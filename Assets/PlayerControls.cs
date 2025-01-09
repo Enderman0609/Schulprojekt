9db456038aad46b81545bbc00a9b1144ad1337ab
Enderman0609 <hannes.segebarth@gmail.com>
2025-01-09 18:03:40 +0100

    Spieler Renn Animation

--- Assets/PlayerControls.cs
+++ Assets/PlayerControls.cs
@@ -7,6 +7,7 @@ public class PlayerCtrl : MonoBehaviour
     private Animator animator;
     public float moveSpeed;
     float speedX, speedY;
+    private bool isMoving;
     Rigidbody2D rb;
     // Start is called before the first frame update
     private void Awake()
@@ -27,5 +28,7 @@ public class PlayerCtrl : MonoBehaviour
         rb.velocity = new Vector2(speedX * moveSpeed, speedY * moveSpeed);
         animator.SetFloat("moveX", speedX);
         animator.SetFloat("moveY", speedY);
+        isMoving = speedX != 0 || speedY != 0;
+        animator.SetBool("isMoving", isMoving);
     }
 }
